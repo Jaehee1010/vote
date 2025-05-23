17137a19ea0a7f6f1220232390fec60c7072c632
CHOICHANHEE03 <1472580q@naver.com>
2025-05-24 01:40:39 +0900

    로고 추가 및 네비바 css 수정, 후보자 등록 추가

--- src/frontend/component/NavBar.js
+++ src/frontend/component/NavBar.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
-
+import logo from '../images/vote_logo.png'
 import '../css/NavBar.css'
 
 
 const Navbar = () => {
     return(
         <nav className="navbar">
-            
+            <div className="nav-logo">
+                <img src = {logo} alt="logo"></img>
+            </div>
             <div className="nav-links">
                 <Link to = "/List">후보자 공약</Link>
                 <Link to = "/Vote">투표하기</Link>
